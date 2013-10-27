fa81b1b4be3373528f556943b838ef16a617e3b6
Ricardo jorge Fideles junior <Ricardo@MacBook-Pro-de-Ricardo.local>
2013-10-27 10:19:48 -0200

    Initial Commit

---
+++ RfSalmosApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RfSalmosApp
+//
+//  Created by Ricardo jorge Fideles junior on 27/10/13.
+//  Copyright (c) 2013 Ricardo jorge Fideles junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RfSalmosApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RfSalmosApp
+//
+//  Created by Ricardo jorge Fideles junior on 27/10/13.
+//  Copyright (c) 2013 Ricardo jorge Fideles junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
