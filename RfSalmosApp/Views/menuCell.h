c9e07580369e2ae09d74f58f15b989de8c14e7bd
Ricardo <ricardo@ricardofideles.com.br>
2013-11-05 13:32:10 -0200

    Adicionando view para o menuCell

---
+++ RfSalmosApp/Views/menuCell.h
@@ -0,0 +1,13 @@
+//
+//  menuCell.h
+//  RfSalmosApp
+//
+//  Created by Ricardo jorge Fideles junior on 05/11/13.
+//  Copyright (c) 2013 Ricardo jorge Fideles junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface menuCell : UITableViewCell
+
+@end
---
+++ RfSalmosApp/Views/menuCell.m
@@ -0,0 +1,29 @@
+//
+//  menuCell.m
+//  RfSalmosApp
+//
+//  Created by Ricardo jorge Fideles junior on 05/11/13.
+//  Copyright (c) 2013 Ricardo jorge Fideles junior. All rights reserved.
+//
+
+#import "menuCell.h"
+
+@implementation menuCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
